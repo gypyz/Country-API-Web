294abc9dcc7afd71eb25c38b26fc575d99460559
gypyz <141420777+gypyz@users.noreply.github.com>
2024-02-12 17:27:33 +0700

    ff

--- script.js
+++ script.js
@@ -3,32 +3,33 @@
 const countries = document.querySelector(".countries");
 
 const getCountry = function (country) {
-	// console.log(country);
-	const req = new XMLHttpRequest();
-	req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-	req.send();
+  // console.log(country);
+  const req = new XMLHttpRequest();
+  req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  req.send();
 
-	req.addEventListener("load", function () {
-		const [data] = JSON.parse(this.responseText);
-		const lang = Object.entries(data.languages);
-		const currency = Object.entries(data.currencies);
-		console.log(data.borders);
-		let allLang = "";
-		let borders = "";
-		if (data.borders == null) {
-			borders = "-";
-		} else {
-			borders = data.borders;
-		}
-		for (let i = 0; i < lang.length; i++) {
-			if (i == lang.length - 1) {
-				allLang = allLang + lang[i][1];
-			} else {
-				allLang = allLang + lang[i][1] + ", ";
-			}
-		}
-		console.log(allLang);
-		const html = `
+  req.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    const lang = Object.entries(data.languages);
+    const currency = Object.entries(data.currencies);
+    console.log(data.borders);
+    let allLang = "";
+    let borders = "";
+    if (data.borders == null) {
+      borders = "-";
+    } else {
+      borders = data.borders;
+    }
+    for (let i = 0; i < lang.length; i++) {
+      if (i == lang.length - 1) {
+        allLang = allLang + lang[i][1];
+      } else {
+        allLang = allLang + lang[i][1] + ", ";
+      }
+    }
+    console.log(allLang);
+    console.log(currency[0][1].symbol);
+    const html = `
         <article class="country">
             <img src="${data.flags.png}" alt="" class="country_img" />
             <div class="country_data">
@@ -43,29 +44,29 @@ const getCountry = function (country) {
             </div>
         </article>`;
 
-		countries.insertAdjacentHTML("beforeend", html);
-		countries.style.opacity = 1;
-		const regionClick = document.querySelector(".regionClick");
-		regionClick.addEventListener("click", (e) => {
-			// console.log(data.region);
-			getRegion(data.region);
-		});
-	});
+    countries.insertAdjacentHTML("beforeend", html);
+    countries.style.opacity = 1;
+    const regionClick = document.querySelector(".regionClick");
+    regionClick.addEventListener("click", (e) => {
+      // console.log(data.region);
+      getRegion(data.region);
+    });
+  });
 };
 
 const getRegion = function (region) {
-	const req = new XMLHttpRequest();
-	req.open("GET", `https://restcountries.com/v3.1/region/${region}`);
-	req.send();
-	req.addEventListener("load", function () {
-		const data = JSON.parse(this.responseText);
+  const req = new XMLHttpRequest();
+  req.open("GET", `https://restcountries.com/v3.1/region/${region}`);
+  req.send();
+  req.addEventListener("load", function () {
+    const data = JSON.parse(this.responseText);
 
-		console.log(data[0].name.official);
-		console.log(data.length);
-		for (let i = 0; i < data.length; i++) {
-			getCountry(data[i].name.official);
-		}
-	});
+    console.log(data[0].name.official);
+    console.log(data.length);
+    for (let i = 0; i < data.length; i++) {
+      getCountry(data[i].name.official);
+    }
+  });
 };
 
 // getCountry("Thailand");
@@ -77,12 +78,12 @@ const searchButton = document.querySelector(".search-button");
 const countriesList = document.querySelector(".countries-list");
 
 searchButton.addEventListener("click", (e) => {
-	e.preventDefault();
-	countries.innerHTML = "";
-	const searchTerm = searchField.value.toLowerCase();
-	getCountry(searchTerm);
+  e.preventDefault();
+  countries.innerHTML = "";
+  const searchTerm = searchField.value.toLowerCase();
+  getCountry(searchTerm);
 
-	console.log(searchTerm);
+  console.log(searchTerm);
 });
 
 // getRegion("asia");
