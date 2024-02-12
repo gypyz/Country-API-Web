3ceddde8cb67fc4b1e3a0df6f8672b2a32278ddc
Sealed <korn2400@gmail.com>
2024-02-12 14:16:47 +0700

    update

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 "use strict";
+
 const countries = document.querySelector(".countries");
 
 const getCountry = function (country) {
-  console.log(country);
-  const req = new XMLHttpRequest();
-  req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  req.send();
-  req.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    const lang = Object.entries(data.languages);
-    const curren = Object.entries(data.currencies);
-    let alllangs = "";
-    for (let i = 0; i < lang.length; i++) {
-      alllangs = alllangs + " " + lang[i][1];
-    }
-    console.log(alllangs);
+	// console.log(country);
+	const req = new XMLHttpRequest();
+	req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+	req.send();
 
-    const html = `<article class="country">
-<img src="${data.flags.png}" alt="thai" class="country__img" />
-<div class="country__data">
-  <h3 class="country__name">${data.name.common}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>🗣️ ${alllangs}</span></p>
-  <p class="country__row"><span>💰${curren[0][1].name}  
-    (${curren[0][1].symbol})
-    </span></p>
-</div>
-</article>`;
-
-    countries.insertAdjacentHTML("beforeend", html);
-  });
+	req.addEventListener("load", function () {
+		const [data] = JSON.parse(this.responseText);
+		const lang = Object.entries(data.languages);
+		const currency = Object.entries(data.currencies);
+		console.log(data.borders);
+		let allLang = "";
+		let borders = "";
+		if (data.borders == null) {
+			borders = "-";
+		} else {
+			borders = data.borders;
+		}
+		for (let i = 0; i < lang.length; i++) {
+			if (i == lang.length - 1) {
+				allLang = allLang + lang[i][1];
+			} else {
+				allLang = allLang + lang[i][1] + ", ";
+			}
+		}
+		console.log(allLang);
+		const html = `
+        <article class="country">
+            <img src="${data.flags.png}" alt="" class="country_img" />
+            <div class="country_data">
+                <h3 class="country_name">${data.name.common}</h3>
+                <h4 class="country_region">${data.region}</h4>
+                <p class="country_row"><span>🗣️</span>${allLang}</p>
+                <p class="country_row"><span>💰</span>${currency[0][1].name} (${currency[0][1].symbol})</p>
+                <p class="country_row"><span>Population</span>${data.population}</p>
+                <p class="country_row"><span>Border</span>${borders}</p>
+                <p class="country_row"><span>Capital</span>${data.capital}</p>
+            </div>
+        </article>`;
+		countries.insertAdjacentHTML("beforeend", html);
+		countries.style.opacity = 1;
+	});
 };
-console.log(this.responseText);
+getCountry("Thailand");
+getCountry("japan");
+getCountry("USA");
+getCountry("Republic of India");
+// const emptySpace = document.querySelector(".space");
 
-getCountry(`Japan`);
-getCountry(`USA`);
-getCountry(`Republic of India`);
-getCountry(`Russia`);
+// const html = `<h2>New Message:</h2><p>this is a message from script</p>`;
+
+// emptySpace.insertAdjacentHTML("beforeend", html);
